dfc5e9c6334eb20a68fc8b90563cfad44605a69a
Alessandra Cardozo dos Santos <alessandracardozodossantos@gmail.com>
2025-10-04 17:44:03 -0300

    Update index.jsx

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,10 +6,11 @@ export default function Footer() {
 		<StyledFooter>
 			<p id="copy">
 				Desenvolvido com muito ❤ e ☕ por{" "}
-				<a href="https://ilrocha.com" target="_blank">
+				<a href="https://github.com/alessandra-cardozo" target="_blank">
 					Alessandra Cardozo
 				</a>
 			</p>
 		</StyledFooter>
 	)
 }
+
